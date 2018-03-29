628ac67b3c6dc3be061ff7933570e880c03ab310
Yeet boi <D227502@edu.rocwb.nl>
2018-03-29 11:04:44 +0200

    Added number to roman numeral converter
    I've added a number converter that converts numbers to roman numerals

--- ShittyFunctionsBot/ShittyFunctionsBot/Form1.Designer.cs
+++ ShittyFunctionsBot/ShittyFunctionsBot/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRomanConverter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRomanConverter
+            // 
+            this.btnRomanConverter.Location = new System.Drawing.Point(21, 31);
+            this.btnRomanConverter.Name = "btnRomanConverter";
+            this.btnRomanConverter.Size = new System.Drawing.Size(140, 23);
+            this.btnRomanConverter.TabIndex = 0;
+            this.btnRomanConverter.Text = "Roman numeral converter";
+            this.btnRomanConverter.UseVisualStyleBackColor = true;
+            this.btnRomanConverter.Click += new System.EventHandler(this.btnRomanConverter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnRomanConverter);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRomanConverter;
     }
 }
 
--- ShittyFunctionsBot/ShittyFunctionsBot/Form1.cs
+++ ShittyFunctionsBot/ShittyFunctionsBot/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
 
 namespace ShittyFunctionsBot
 {
@@ -15,6 +18,12 @@ namespace ShittyFunctionsBot
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRomanConverter_Click(object sender, EventArgs e)
+        {
+            RomanNumeralConverter RomanNumeralConverter = new RomanNumeralConverter();
+            RomanNumeralConverter.Show();    
         }
     }
 }
